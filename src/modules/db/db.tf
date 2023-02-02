85fd95005e892a2298c61bc5f8c17546ceabcae8
Melissa Charles <melissacharles.py@gmail.com>
2023-02-01 19:54:30 -0500

    dB security group added

--- src/modules/db/db.tf
+++ src/modules/db/db.tf
@@ -9,3 +9,22 @@ resource "aws_db_subnet_group" "MelissaDBSubnetGroup" {
      Project = "Melissa Terraform Provision"
    }
 }
+
+resource "aws_security_group" "MelissaDBSubnetGroup" {
+  name = "melissa-db-security-group"
+  vpc_id = var.melissa_vpc_id
+
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    cidr_blocks = [
+        var.melissa_private_subnet_cidrs[0],
+        var.melissa_private_subnet_cidrs[1]
+    ]
+  }
+  tags = {
+    Name = "MelissaDBSecurityGroup"
+    Project = "Melissa Terraform Provision"
+  }
+}
