039b41c380ca44b9dd818c20de89efe61ed20ff7
Melissa Charles <melissacharles.py@gmail.com>
2023-02-03 18:44:37 -0500

    Updated LB listener targer group arn source

--- src/modules/webserver/webservers.tf
+++ src/modules/webserver/webservers.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "MelissaWebserverSecurityGroup" {
 resource "aws_lb" "MelissaLoadBalancer" {
     name = "web-app-lb"
     load_balancer_type = "application"
-    subnets = [var.melissa_public_subnets[0].id, var.cc_public_subnets[1].id]
+    subnets = [var.melissa_public_subnets[0].id, var.melissa_public_subnets[1].id]
     security_groups = [aws_security_group.MelissaWebserverSecurityGroup.id]
     tags = {
       Name = "MelissaLoadBalancer"
@@ -43,7 +43,7 @@ resource "aws_lb_listener" "MelissaLBListerner" {
     protocol = "HTTP"
 
     default_action {
-      target_group_arn = aws_lb_target_group.ccTargetGroup.id
+      target_group_arn = aws_lb_target_group.MelissaTargetGroup.id
       type = "forward"
     }
 }
@@ -83,7 +83,7 @@ resource "aws_lb_target_group_attachment" "webserver2" {
 }
 
 resource "aws_instance" "webserver1" {
-  ami = local.ami.vpc_id
+  ami = local.ami_id
   instance_type = local.instance_type
   key_name = local.key_name
   subnet_id = var.melissa_public_subnets[0].id
@@ -102,7 +102,7 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami = local.ami.vpc_id
+  ami = local.ami_id
   instance_type = local.instance_type
   key_name = local.key_name
   subnet_id = var.melissa_public_subnets[0].id
